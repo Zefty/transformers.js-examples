cc84262db0a44a5ddf4278d711ca63a11f0940a6
Jaime Wu <jaime.wu011@gmail.com>
2025-09-14 09:38:41 +1200

    sdfsdf

--- next-server/app/api/classify/route.js
+++ next-server/app/api/classify/route.js
@@ -5,12 +5,11 @@ export async function GET(request) {
     return Response.json({ message: "No text provided" }, { status: 400 });
   }
 
-  // const { pipeline } = await import("@huggingface/transformers");
-  // const classifier = await pipeline(
-  //   "text-classification",
-  //   "Xenova/distilbert-base-uncased-finetuned-sst-2-english",
-  // );
-  // const result = await classifier(text);
-  // return Response.json(result[0]);
-  return Response.json({})
+  const { pipeline } = await import("@huggingface/transformers");
+  const classifier = await pipeline(
+    "text-classification",
+    "Xenova/distilbert-base-uncased-finetuned-sst-2-english",
+  );
+  const result = await classifier(text);
+  return Response.json(result[0]);
 }
