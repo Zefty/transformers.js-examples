07f7fc0f29c1b29fdc0e20fab422e9e415833776
pengi <jaime.wu011@gmail.com>
2025-09-15 10:30:35 +1200

    dasas

--- next-server/next.config.mjs
+++ next-server/next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ✅ This tells Next.js to NOT bundle these into the output, just require() them at runtime
-  // serverExternalPackages: [
-  //   "@huggingface/transformers",
-  //   "@xenova/transformers",
-  //   "onnxruntime-node",
-  //   "sharp",
-  // ],
-
-
+  serverExternalPackages: [
+    "@huggingface/transformers",
+    "@xenova/transformers",
+    "onnxruntime-node",
+    "sharp",
+  ],
 };
 
 export default nextConfig;
