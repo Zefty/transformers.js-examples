d7d13be1fbac1c59975b76e934efb046fd8b7748
pengi <jaime.wu011@gmail.com>
2025-09-15 09:08:49 +1200

    dfsdf

--- next-client/next.config.mjs
+++ next-client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/output
-  output: "standalone",
+  // output: "standalone",
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/serverExternalPackages
   serverExternalPackages: ["@huggingface/transformers"],
 };
